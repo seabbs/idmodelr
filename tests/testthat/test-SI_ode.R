ccd5a8c3672ff0317558f6a16b87943833c64a59
Sam Abbott <contact@samabbott.co.uk>
2017-12-05 17:24:16 +0000

    Create test-SI_ode.R
    Added simple test

---
+++ tests/testthat/test-SI_ode.R
@@ -0,0 +1,18 @@
+context("SI_ode")
+
+
+
+S_0 <- 999
+I_0 <- 1
+beta <- 3
+
+parameters <- c(beta = beta)
+inits <- c(S = S_0, I = I_0)
+
+test <- SI_ode(1, inits, parameters)
+
+result <- list(c(S = -2.997, S = 2.997))
+
+test_that("SI_ode can correctly return rates of change over one timestep", {
+  expect_equal(result, test)
+})
